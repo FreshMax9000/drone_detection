798f04c01b4fcdf51ba137109138735fe4c3e3ee
FreshMax9000 <maxmnaujokat@gmail.com>
2021-06-01 17:20:24 +0200

    Added attribute pos to ImageData

--- src/image_data.py
+++ src/image_data.py
@@ -1,15 +1,17 @@
 
 import cv2
+import numpy as np
 
 
 class ImageData:
 
     folder_path = "/home/max/Documents/Studienarbeit/DroneSpot/implementation_kek/a_lot_of_images"
 
-    def __init__(self, image, timestamp: float, esp_name: str) -> None:
+    def __init__(self, image, timestamp: float, esp_name: str, pos: np.array) -> None:
         self.image = image
         self.timestamp = timestamp
         self.esp_name = esp_name
+        self.pos = pos
 
     def save_to_file(self):
         cv2.imwrite(self.folder_path+f"/{self.timestamp}-{self.esp_name}.jpg", self.image)
