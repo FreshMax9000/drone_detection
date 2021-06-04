1304fd291683254f87ddb0fadfef8e81a33e865e
FreshMax9000 <maxmnaujokat@gmail.com>
2021-06-04 20:06:13 +0200

    Implemented full detection chain

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import numpy as np
 
 from src.single_erfassung import ThreadController
 from src.single_auswertung import BildAuswertung
+from src.detection import DetectionMaker
+
+
+DISTANCE_THRESHOLD = 0.5
 
 
 def main():
@@ -18,10 +22,11 @@ def main():
     ]
     ESP_32_POS_LIST = [
         np.array([0, 0, 0]),
-        np.array([0, 0.07, 0])
+        np.array([0, 0.06, 0])
     ]
 
     auswertung = BildAuswertung()
+    detector = DetectionMaker(DISTANCE_THRESHOLD)
 
     with ThreadController(ESP_32_URL_LIST, ESP_32_POS_LIST, esp32_name_list=ESP_32_NAME_LIST) as img_threads:
         print("Detection running !")
@@ -32,11 +37,10 @@ def main():
             tmp_time = time.time() - tmp_time
             #print(f"Getting picture dict took {tmp_time}s")
             tmp_time = time.time()
-            for spot in auswertung.get_spotted(image_dict):
-                print(spot)
+            spot_list = auswertung.get_spotted(image_dict)
             tmp_time = time.time() - tmp_time
             #print(f"Analying and printing the detections took {tmp_time}s")
-            # todo further processing
+            print(f"{time.time():.0f}: {detector.make_detection(spot_list, timestamp)}")
 
 
 
